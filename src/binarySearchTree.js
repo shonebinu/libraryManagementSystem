f5639ab6f1439c4e0d1cfd6787ecef63f5f2b94e
Shone Binu <shonebinualias@gmail.com>
2023-10-28 08:14:37 +0530

    added lending, deleting, status functionalities

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,8 +1,9 @@
 "use strict"
 
 class Node {
-  constructor(key) {
+  constructor(key, copies) {
     this.key = key;
+    this.copies = copies;
     this.left = null;
     this.right = null;
   }
@@ -13,8 +14,8 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(key) {
-    const newNode = new Node(key);
+  insert(key, copies = 1) {
+    const newNode = new Node(key, copies);
     if (this.root === null) 
       this.root = newNode;
     else 
@@ -27,13 +28,16 @@ class BinarySearchTree {
         node.left = newNode;
       else 
         this._insertNode(node.left, newNode);
-    }
-
-    else {
-      if (node.right === null) 
+    } 
+    else if (newNode.key > node.key) {
+      if (node.right === null)
         node.right = newNode;
       else 
         this._insertNode(node.right, newNode);
+    }
+
+    else {
+      node.copies += newNode.copies;
     }
   }
   
@@ -53,6 +57,57 @@ class BinarySearchTree {
       return true;
 
   }
+
+    lendBook(bookName, numCopies = 1) {
+    const foundNode = this._findNode(this.root, bookName);
+    if (foundNode && foundNode.copies >= numCopies) {
+      foundNode.copies -= numCopies;
+      console.log(`Successfully lent ${numCopies} copies of ${bookName}.`);
+    } else if (foundNode && foundNode.copies < numCopies) {
+      console.log(`Sorry, only ${foundNode.copies} copies of ${bookName} are available.`);
+    } else {
+      console.log(`${bookName} not found in the library.`);
+    }
+  }
+
+  returnBook(bookName, numCopies = 1) {
+    const foundNode = this._findNode(this.root, bookName);
+    if (foundNode) {
+      foundNode.copies += numCopies;
+      console.log(`Successfully returned ${numCopies} copies of ${bookName}.`);
+    } else {
+      console.log(`${bookName} not found in the library.`);
+    }
+  }
+
+  checkBookStatus(bookName) {
+    const foundNode = this._findNode(this.root, bookName);
+    if (foundNode) {
+      console.log(`${bookName} - Available Copies: ${foundNode.copies}`);
+    } else {
+      console.log(`${bookName} not found in the library.`);
+    }
+  }
+
+  _findNode(node, key) {
+    if (node === null) return null;
+    if (key < node.key) return this._findNode(node.left, key);
+    else if (key > node.key) return this._findNode(node.right, key);
+    else return node;
+  }
+
+  printLibraryStatus() {
+    this._inOrderTraversal(this.root);
+  }
+
+  _inOrderTraversal(node) {
+    if (node !== null) {
+      this._inOrderTraversal(node.left);
+      console.log(`Book: ${node.key} - Available Copies: ${node.copies}`);
+      this._inOrderTraversal(node.right);
+    }
+  }
+
 }
 
 
